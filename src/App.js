bdeaa354a3866f63d19f0d978c141f4e6c5c849e
Igor Klyopov <shoot8202@gmail.com>
2021-08-15 08:48:01 +0300

    adds react-app template

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Container from "./components/Container/Container";
+
+function App() {
+  return (
+    <>
+      <Container>
+        <h1 className="testTitle">Hello everyone!</h1>
+        {/* for test */}
+      </Container>
+    </>
+  );
+}
+
+export default App;
